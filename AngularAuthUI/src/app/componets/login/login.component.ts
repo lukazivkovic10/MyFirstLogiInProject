41154a99bbea2ca25c9626aebd14d0c0fdb31548
Luka <luka.zivkovic007@gmail.com>
2023-06-26 16:39:28 +0200

    Dodal pogled gesla ki ga vpisujes

--- AngularAuthUI/src/app/componets/login/login.component.ts
+++ AngularAuthUI/src/app/componets/login/login.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye";
 
+  hideShowPassword()
+  {
+    this.isText = !this.isText;//Ker je isText Boolean naredi to true or false
+    this.isText ? this.eyeIcon = "fa-eye-slash" : this.eyeIcon = "fa-eye"; //Če je true naredi icon v eye-slash drugace pa icon v fa-eye
+    this.isText ? this.type = "text" : this.type = "password"; //to pa če je true spremeni type v text drugače pa v password
+  };
 }
