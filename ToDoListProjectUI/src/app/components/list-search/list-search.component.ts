2bf7b3c9403e77f420dea55e391df6c8e1d6e258
Luka <luka.zivkovic007@gmail.com>
2023-07-04 22:31:14 +0200

    Iskalnik v UI

--- ToDoListProjectUI/src/app/components/list-search/list-search.component.ts
+++ ToDoListProjectUI/src/app/components/list-search/list-search.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-list-search',
   templateUrl: './list-search.component.html',
   styleUrls: ['./list-search.component.css']
 })
-export class ListSearchComponent {
+export class ListSearchComponent implements OnInit  {
+  searchForm!: FormGroup;
+  constructor(private fb: FormBuilder) {};
+  ngOnInit(): void {
+    this.searchForm = this.fb.group({
+      search: ['']
+    })
+  }
+
+  onSearch(){
+    console.log(this.searchForm.value);
+  }
 
 }
