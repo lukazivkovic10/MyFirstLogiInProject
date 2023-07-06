f0dd61eb4e352824435f35160fddda7cd7c9fe65
Luka <luka.zivkovic007@gmail.com>
2023-07-06 14:47:21 +0200

    Naredil API

---
+++ ToDoListAPI/ToDoListAPI/Controllers/ItemsController.cs
@@ -0,0 +1,80 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Reflection.Metadata.Ecma335;
+using ToDoListAPI.Models;
+
+namespace ToDoListAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public ItemsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet("IskanjeVseh")]
+        public async Task<ActionResult<List<Items>>> GetAllItems()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            IEnumerable<Items> items = await SelectAllItems(connection);
+            return Ok(items);
+        }
+
+        [HttpGet("IskanjeLista{ItemTag}")]
+        public async Task<ActionResult<List<Items>>> GetItems(string ItemTag)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var item = await connection.QueryAsync<Items>("select * from Items where Tag = @Tag",
+                new { Tag = ItemTag });
+            return Ok(item);
+        }
+
+        [HttpPost("Ustvarjanje")]
+        public async Task<ActionResult<List<Items>>> CreateItem(Items items)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var item = await connection.ExecuteAsync("insert into Items (Tag, ItemName, ItemDesc) values (@Tag, @ItemName, @ItemDesc)", items);
+            return Ok(item);
+        }
+
+        [HttpPut("Posodobitev")]
+
+        public async Task<ActionResult<List<Items>>> UpdateItem(Items items)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("update Items set ItemName = @ItemName, ItemDesc = @ItemDesc where Tag = @Tag", items);
+            return Ok(await SelectAllItems(connection));
+        }
+
+        [HttpDelete("Opravljeno")]
+
+        public async Task<ActionResult<List<Items>>> DoneItem(string ItemName)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("update Items set Active = '0' where ItemName = @ItemName",
+                new { ItemName = ItemName });
+            return Ok(await SelectAllItems(connection));
+        }
+
+        [HttpDelete("SoftDelete,{ItemName}")]
+
+        public async Task<ActionResult<List<Items>>> SoftDeleteItem(string ItemName)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("update Items set ItemStatus = '0' where ItemName = @ItemName",
+                new { ItemName = ItemName });
+            return Ok(await SelectAllItems(connection));
+        }
+
+        private static async Task<IEnumerable<Items>> SelectAllItems(SqlConnection connection)
+        {
+            return await connection.QueryAsync<Items>("select * from Items");
+        }
+    }
+}
---
+++ ToDoListAPI/ToDoListAPI/Models/Items.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListAPI.Models
+{
+    public class Items
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Tag { get; set; }
+        public string ItemName { get; set; }
+        public string ItemDesc { get; set; }
+        public int ItemStatus { get; set; } = 1;
+        public int Active { get; set; } = 1;
+
+    }
+}
