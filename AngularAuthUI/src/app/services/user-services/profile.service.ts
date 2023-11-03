ccb3b9622969261b72ca76c33d63f3b4d69ea0ef
Luka <luka.zivkovic007@gmail.com>
2023-11-03 22:52:38 +0100

    Fixed userProfile services

--- AngularAuthUI/src/app/services/user-services/profile.service.ts
+++ AngularAuthUI/src/app/services/user-services/profile.service.ts
@@ -8,7 +8,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class ProfileService {
-  private baseUrl:string = environment.apiUrl+'userProfile/';
+  private baseUrl:string = environment.apiUrl;
   constructor(private http : HttpClient, private router: Router, private tokenService: TokenServiceService) { }
   private addTokenToRequest(url: string, params: HttpParams = new HttpParams()) {
     const headers = this.tokenService.getRequestHeaders();
